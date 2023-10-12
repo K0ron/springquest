51d48ac8b27e8a1a4cebe42ce7e4ab33e196a072
Kevin Caron <kevin.caron.91@gmail.com>
2023-10-12 17:26:21 +0200

    Initial commit

---
+++ src/main/java/com/wildcodeschool/springquest/SpringquestApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wildcodeschool/springquest/SpringquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringquestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
